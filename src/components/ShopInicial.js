c0465a90f2eb50e1e329a154119fa19d3c10e01e
brenda <brendaden767@gmail.com>
2022-03-23 07:49:42 -0300

    Entrega contador

--- src/components/ShopInicial.js
+++ src/components/ShopInicial.js
@@ -1,16 +1,17 @@
 import React, {useState} from "react";
 import { Button, Card} from 'react-bootstrap';
 
-const ShopInicial = (props) =>{
-let [contador, setContador]= useState(0)
-let [stock, setStock]= useState(10);
+const ShopInicial = ({stock, initial, onAdd}) =>{
+let [contador, setContador]= useState(initial);
+let [cant, setStock]= useState(stock);
 
 function sumar(){
-    if(contador<=stock){
+    if(contador<=cant){
       setContador(contador+1)
+
     }
-    else{
-        alert("sin stock")
+    else if(contador=cant){
+      return
     }
 };
 
@@ -18,24 +19,27 @@ function restar(){
     if(contador<=0){
         return
     }
-    else{
+    else if(contador>=0){
         setContador(contador-1)
-        setStock(stock+1)
     }    
 };
+const addToCart=()=>{
+  onAdd();
+}
 
   return (
     <div className='card'>
                     <Card style={{ width: '18rem'}} className="contenedor-card">
             <Card.Img variant="top" src='https://img2.cgtrader.com/items/3050459/60232a08f5/large/jujutsu-kaisen-ryomen-sukuna-3d-model-stl.jpg' />
             <Card.Body>
-                <Card.Title>{props.name} </Card.Title>
-                <Card.Text>${props.price} </Card.Text>
+                <Card.Title>{/* {props.name} */}titulo </Card.Title>
+                <Card.Text>${/* {props.price} */}precio </Card.Text>
              </Card.Body>   
          <h3>{contador}</h3>
          <h4>{stock}</h4>
          <Button variant="primary" className="btn-contador" onClick={sumar} >Agregar</Button>
          <Button variant="primary" className="btn-contador" onClick={restar} >Eliminar</Button> 
+         <Button variant="primary" className="btn-contador" onClick={addToCart} >ADD</Button> 
     </Card> 
     </div>
 
--- src/components/itemlistContainer.js
+++ src/components/itemlistContainer.js
@@ -1,23 +1,37 @@
 import React, { useState } from 'react';
 import Shopinitial from './ShopInicial'
 
+const stock=10;
+const initial=1;
+
+function onAdd(){
+    console.log("click")
+}
 
 const ItemListContainer = () => {
-    const [products, setProducts]= useState(
-        [
-            {id:1, name:"cortante", price:100, stock:10},
-            {id:2, name:"iron man", price:800,stock:10},
-            {id:3, name:"maceta", price:300,stock:10},
-        ])
-    ;
 
 return (
     <div className='containerCard'>
-    {products.map((product)=>{
-        return <Shopinitial name={product.name} price={product.price} stock={10} />
-    })}</div>
+     <Shopinitial  stock={stock} initial={initial} onAdd={onAdd} />
+    </div>
+
 );
     
 }
 export default ItemListContainer; 
 
+
+/* const [products, setProducts]= useState(
+    [
+        {id:1, name:"cortante", price:100},
+        {id:2, name:"iron man", price:800},
+        {id:3, name:"maceta", price:300},
+    ])
+;
+
+return (
+<div className='containerCard'>
+{products.map((product)=>{
+    return <Shopinitial name={product.name} price={product.price} stock={stock} initial={initial} onAdd={onAdd} />
+})}</div>
+); */
