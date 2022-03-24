e80d048762085b3a13809d572f2d557083cde34c
brenda <brendaden767@gmail.com>
2022-03-24 15:13:54 -0300

    "entrega de correciones en contador"

--- src/components/itemlist.js
+++ src/components/itemlist.js
@@ -4,7 +4,7 @@ import Item from './Items'
 const listItem=({lista})=>{
     return(
     <div className='containerCard'>
-    {lista.map((product)=><Item product={product} key={product.id} />)}
+    {lista.map((product)=><Item product={product} key={product.id}/>)}
     </div>
     )
 }
