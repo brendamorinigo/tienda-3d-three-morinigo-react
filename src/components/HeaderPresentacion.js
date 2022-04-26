3af9b6ae730834c6d4ed0976c2e18206ec334a74
brenda <brendaden767@gmail.com>
2022-04-26 12:50:37 -0300

    Entrega final

--- src/components/HeaderPresentacion.js
+++ src/components/HeaderPresentacion.js
@@ -2,15 +2,15 @@ import react from "react"
 
 function HeaderPresentacion() { 
     return(
-        <div>
+        <div className="container-header">
         <div className="contendor-presentacion">
-            <h1>Bienvenido a THREE</h1>
-            <p>Nuestro equipo técnico te puede guiar con modificaciones de diseño, materiales y
+            <h1 className="container-text">Bienvenido a THREE</h1>
+            <p className="container-text">Nuestro equipo técnico te puede guiar con modificaciones de diseño, materiales y
                 configuración que maximizan las probabilidades de éxito de la pieza final y 
                 así ayudarte a llevar una idea de la primera prueba a una producción importante...
                 Estamos a disposición, contamos tu proyecto y lo que necesitás realizar.</p>
         </div>
-        <div className="separador"><p></p> </div>
+        <div className="separador"><p></p></div>
         </div>
     )
  }
